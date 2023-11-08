48f29e2df13493f25243017011939ee5bdc38fb4
Dominik Jelinek <dominik.jelinek@any-3.com>
2023-11-08 05:52:25 +0100

    Rust - embed assets into the Rocket server

--- Rust/hello-embed-rocket/src/main.rs
+++ Rust/hello-embed-rocket/src/main.rs
@@ -4,6 +4,16 @@ use rocket::http::{ContentType, Status};
 #[macro_use]
 extern crate rocket;
 
+use std::borrow::Cow;
+use std::ffi::OsStr;
+use std::path::PathBuf;
+
+use rust_embed::RustEmbed;
+
+#[derive(RustEmbed)]
+#[folder = "static/"]
+struct Asset;
+
 #[get("/hello")]
 fn hello() -> &'static str {
     "Hello, world!"
@@ -17,9 +27,21 @@ fn json() -> (Status, (ContentType, &'static str)) {
     )
 }
 
+#[get("/file/<file..>")]
+fn file(file: PathBuf) -> Option<(ContentType, Cow<'static, [u8]>)> {
+    let filename = file.display().to_string();
+    let asset = Asset::get(&filename)?;
+    let content_type = file
+        .extension()
+        .and_then(OsStr::to_str)
+        .and_then(ContentType::from_extension)
+        .unwrap_or(ContentType::Bytes);
+
+    Some((content_type, asset.data))
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build()
-        .mount("/", routes![hello, json])
-        .mount("/file", FileServer::from(relative!("static")))
+    rocket::build().mount("/", routes![hello, json, file])
+    // .mount("/file", FileServer::from(relative!("static")))
 }

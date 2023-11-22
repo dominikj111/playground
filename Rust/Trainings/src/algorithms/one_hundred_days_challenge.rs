27db57940feac309ddbf9cd9f59ccb0e13c321a2
Dominik Jelinek <dominik.jelinek@any-3.com>
2023-11-23 00:43:42 +0100

    Rust Trainings - finish first series of '100 days challenge'

--- Rust/Trainings/src/algorithms/one_hundred_days_challenge.rs
+++ Rust/Trainings/src/algorithms/one_hundred_days_challenge.rs
@@ -147,7 +147,8 @@ pub fn get_pascal_triangle(n: usize) -> Vec<Vec<i32>> {
 }
 
 /**
- * Given an array A[] consisting of only 0s, 1s, and 2s. The task is to write a function that sorts the given array. The functions should put all 0s first, then all 1s and all 2s in last.
+ * Given an array A[] consisting of only 0s, 1s, and 2s. The task is to write a function that sorts the given array. The functions should put all 0s first,
+ * then all 1s and all 2s in last.
  * This problem is also the same as the famous “Dutch National Flag problem”. The problem was proposed by Edsger Dijkstra. The problem is as follows:
  *
  * Example:
@@ -155,9 +156,27 @@ pub fn get_pascal_triangle(n: usize) -> Vec<Vec<i32>> {
  * Output: {0, 0, 1, 1, 2, 2}
  *
  * https://dev.to/pahujanayan/100-days-day-4-48ch
+ *
+ * I would to implement this by making custom collection what will encapsulate straight grouping according to
+ * element we want to add. We will need an API to act as a single sorted collection and we can avoid sorting.
+ * We can implement function to convert to an array or a vector.
+ * Only processing we will need is to decide what group we want to use for each element and
+ * copy all elements over into collection of all when needed.
+ *
+ * But this is sorting problem, so lets sort it!
  */
-pub fn dutch_national_flag_algorithm(matrix: &mut Vec<Vec<i32>>) {
-    todo!();
+pub fn dutch_national_flag_algorithm(numbers: &mut Vec<i32>) {
+    // Timsort, O(n log n)
+    numbers.sort();
+
+    // Bubble sort, O(n^2)
+    for i in 0..(numbers.len() - 1) {
+        for j in (i + 1)..numbers.len() {
+            if numbers[i] > numbers[j] {
+                numbers.swap(i, j);
+            }
+        }
+    }
 }
 
 /**
@@ -176,8 +195,28 @@ pub fn dutch_national_flag_algorithm(matrix: &mut Vec<Vec<i32>>) {
  *
  * https://dev.to/pahujanayan/100-days-day-5-4p6a
  */
-pub fn best_time_to_buy_and_sell_stock(prices: &mut Vec<i32>) {
-    todo!();
+pub fn best_time_to_buy_and_sell_stock(prices: &[i32]) -> i32 {
+    // Time complexity: O(n)
+    let mut min_price = std::i32::MAX;
+    let mut max_profit = 0;
+
+    for price in prices {
+        min_price = std::cmp::min(min_price, *price);
+        max_profit = std::cmp::max(max_profit, price - min_price);
+    }
+
+    max_profit
+
+    // Time complexity: O(n^2)
+    // let mut profit = 0;
+    // for i in 0..(prices.len() - 1) {
+    //     let purchased_for = prices[i];
+    //     for j in (i + 1)..prices.len() {
+    //         let soldable_for = prices[j];
+    //         profit = std::cmp::max(profit, soldable_for - purchased_for);
+    //     }
+    // }
+    // profit
 }
 
 /**
@@ -186,13 +225,20 @@ pub fn best_time_to_buy_and_sell_stock(prices: &mut Vec<i32>) {
  * DO NOT allocate another 2D matrix and do the rotation.
  *
  * Example:
- * Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
- * Output: [[7,4,1],[8,5,2],[9,6,3]]
+ * Input: matrix = `[[1,2,3],[4,5,6],[7,8,9]]`
+ * Output: `[[7,4,1],[8,5,2],[9,6,3]]`
  *
  * https://dev.to/pahujanayan/100-days-day-5ii-5340
  */
 pub fn rotate_image(matrix: &mut Vec<Vec<i32>>) {
-    todo!();
+    let cloned = matrix.clone();
+    let n = matrix.len();
+
+    for i in 0..n {
+        for j in 0..n {
+            matrix[i][j] = cloned[n - 1 - j][i];
+        }
+    }
 }
 
 #[cfg(test)]
@@ -234,22 +280,53 @@ mod tests {
         assert_eq!(super::get_pascal_triangle(0), vec![] as Vec<Vec<i32>>);
         assert_eq!(super::get_pascal_triangle(1), vec![vec![1]]);
         assert_eq!(super::get_pascal_triangle(2), vec![vec![1], vec![1, 1]]);
-        assert_eq!(super::get_pascal_triangle(5), vec![vec![1], vec![1, 1], vec![1, 2, 1], vec![1, 3, 3, 1], vec![1, 4, 6, 4, 1]]);
+        assert_eq!(
+            super::get_pascal_triangle(5),
+            vec![
+                vec![1],
+                vec![1, 1],
+                vec![1, 2, 1],
+                vec![1, 3, 3, 1],
+                vec![1, 4, 6, 4, 1]
+            ]
+        );
         assert_eq!(super::get_pascal_triangle(9)[8][4], 70);
     }
 
     #[test]
     fn dutch_national_flag_algorithm_test() {
-        todo!();
+        let mut vec1 = vec![0, 1, 2, 0, 1, 1];
+        super::dutch_national_flag_algorithm(&mut vec1);
+        assert_eq!(vec1, vec![0, 0, 1, 1, 1, 2]);
+
+        let mut vec2 = vec![0, 0, 2, 1, 2, 1];
+        super::dutch_national_flag_algorithm(&mut vec2);
+        assert_eq!(vec2, vec![0, 0, 1, 1, 2, 2]);
     }
 
     #[test]
     fn best_time_to_buy_and_sell_stock_test() {
-        todo!();
+        assert_eq!(
+            super::best_time_to_buy_and_sell_stock(&[7, 1, 5, 3, 6, 4]),
+            5
+        );
+
+        assert_eq!(
+            super::best_time_to_buy_and_sell_stock(&[7, 8, 9, 10, 11, 12]),
+            5
+        );
+
+        assert_eq!(
+            super::best_time_to_buy_and_sell_stock(&[12, 11, 10, 9, 8, 7]),
+            0
+        );
     }
 
     #[test]
     fn rotate_image_test() {
-        todo!();
+        let mut matrix = vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]];
+
+        super::rotate_image(&mut matrix);
+        assert_eq!(matrix, vec![vec![7, 4, 1], vec![8, 5, 2], vec![9, 6, 3]]);
     }
 }

25049e8473632120d2e6cae6759fd5dacef64aa9
Dominik Jelinek <dominik.jelinek@any-3.com>
2024-04-27 07:41:11 +0100

    Rust - upgrade bevy to 0.13.2

--- Rust/bevy/examples/entrypoint/src/cli.rs
+++ Rust/bevy/examples/entrypoint/src/cli.rs
@@ -3,7 +3,7 @@ use clap::Parser;
 #[derive(Parser)]
 #[command(version)]
 pub struct Args {
-    /// App name to run. Available app names: 'hello_shapes', 'bloom_2d'
+    /// App name to run. Available app names: 'hello_shapes', 'bloom_2d', 'mesh_2d'
     #[arg(long, default_value = "hello_shapes")]
     pub appname: String,
 }
